d3180f58323ee0e003d8b767f0df5abb429c33c4
Alex Krantz <alex@alexkrantz.com>
2020-03-14 14:25:24 -0700

    Use thiserror crate per #2

--- src/errors.rs
+++ src/errors.rs
@@ -1,78 +1,35 @@
-use std::{error, fmt, io};
+use std::io;
+use thiserror::Error;
 use url::ParseError;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ConfigError {
-    InvalidUrl(ParseError),
+    #[error("Invalid URL: {0}")]
+    InvalidUrl(#[from] ParseError),
+    #[error("Invalid authentication token")]
     InvalidToken,
+    #[error("Configuration file does not exist")]
     NonExistentConfigFile,
-    ReadError(io::Error),
-    JsonError(serde_json::Error),
-    CertificateDecodeError(reqwest::Error),
+    #[error("Failed to read from file: {0}")]
+    ReadError(#[from] io::Error),
+    #[error("Failed to decode JSON: {0}")]
+    JsonError(#[from] serde_json::Error),
+    #[error("Failed to decode certificate: {0}")]
+    CertificateDecodeError(#[from] reqwest::Error),
 }
 
-impl fmt::Display for ConfigError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ConfigError::InvalidUrl(e) => write!(f, "Invalid URL: {}", e),
-            ConfigError::InvalidToken => write!(f, "Invalid authentication token"),
-            ConfigError::NonExistentConfigFile => write!(f, "Configuration file does not exist"),
-            ConfigError::ReadError(e) => write!(f, "Failed to read from file: {}", e),
-            ConfigError::JsonError(e) => write!(f, "Failed to decode JSON: {}", e),
-            ConfigError::CertificateDecodeError(e) => {
-                write!(f, "Failed to decode certificate: {}", e)
-            }
-        }
-    }
-}
-
-impl error::Error for ConfigError {}
-
-impl From<io::Error> for ConfigError {
-    fn from(error: io::Error) -> Self {
-        ConfigError::ReadError(error)
-    }
-}
-
-impl From<serde_json::Error> for ConfigError {
-    fn from(error: serde_json::Error) -> Self {
-        ConfigError::JsonError(error)
-    }
-}
-
-impl From<reqwest::Error> for ConfigError {
-    fn from(error: reqwest::Error) -> Self {
-        ConfigError::CertificateDecodeError(error)
-    }
-}
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ApiError {
+    #[error("Server error")]
     ServerError,
+    #[error("Permission denied")]
     PermissionDenied,
+    #[error("Unknown role")]
     UnknownRole,
+    #[error("Invalid public key format")]
     InvalidPublicKey,
-    SendFailure(reqwest::Error),
+    #[error("Failed to send request: {0}")]
+    SendFailure(#[from] reqwest::Error),
+    #[error("An unknown error occurred")]
     UnknownError,
-}
-
-impl fmt::Display for ApiError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ApiError::ServerError => write!(f, "Server error"),
-            ApiError::PermissionDenied => write!(f, "Permission denied"),
-            ApiError::UnknownRole => write!(f, "Unknown role"),
-            ApiError::InvalidPublicKey => write!(f, "Invalid public key format"),
-            ApiError::SendFailure(e) => write!(f, "Failed to send request: {}", e),
-            ApiError::UnknownError => write!(f, "An unknown error occurred"),
-        }
-    }
-}
-
-impl error::Error for ApiError {}
-
-impl From<reqwest::Error> for ApiError {
-    fn from(error: reqwest::Error) -> Self {
-        ApiError::SendFailure(error)
-    }
 }
